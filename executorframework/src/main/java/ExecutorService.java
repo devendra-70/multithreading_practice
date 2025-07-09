b5bf4ec9f407a0915da04036ba514668de2bc7a7
devendra-70 <devendramahajan70@gmail.com>
2025-07-09 18:16:16 +0530

    added thread array

--- executorframework/src/main/java/ExecutorService.java
+++ executorframework/src/main/java/ExecutorService.java
@@ -1,10 +1,43 @@
 public class ExecutorService {
     public static void main(String[] args){
 
-        //ex:factorial synchrnouly
+        //ex1:factorial synchrnouly
+        long startTime=System.currentTimeMillis();
         for(int i=1;i<10;i++){
             System.out.println(factorial(i));
         }
+        System.out.println("Total time:"+(System.currentTimeMillis()-startTime));
+
+
+
+
+        //ex2: use multithreading
+        long startTime1=System.currentTimeMillis();
+        //array of threads
+        Thread[] threads=new Thread[9];
+        for(int i=1;i<10;i++){
+            int finalI=i;
+             threads[i-1] = new Thread(
+                    () ->{
+                        long result = factorial(finalI);
+                        System.out.println(result);
+                    }
+            );
+            threads[i-1].start();
+        }
+
+        /*we have to join all threads to main before executing below statement
+        to get correct timing.*/
+
+        for(Thread thread:threads){
+            try{
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Total time with threads:"+(System.currentTimeMillis()-startTime1));
+
     }
 
     private static long factorial(int n){
